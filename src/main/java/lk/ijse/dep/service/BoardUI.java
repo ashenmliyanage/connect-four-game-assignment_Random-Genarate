46c384110e305fd4a099296c1a649cf5e6b88dc3
Ashen Madhushanka <139870301+ashenmliyanage@users.noreply.github.com>
2023-10-06 21:29:33 +0530

    Delete src/main/java/lk/ijse/dep/service directory
    Update to new

--- src/main/java/lk/ijse/dep/service/BoardUI.java
+++
@@ -1,6 +0,0 @@
-package lk.ijse.dep.service;
-
-public interface BoardUI {
-    public void update(int col, boolean isHuman);
-    public void notifyWinner(Winner winner);
-}
--- src/main/java/lk/ijse/dep/service/Piece.java
+++
@@ -1,5 +0,0 @@
-package lk.ijse.dep.service;
-
-public enum Piece {
-    GREEN,BLUE,EMPTY;
-}
